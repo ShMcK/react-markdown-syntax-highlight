fc1bea872ccdca91fe4a9f697c1b9912cad96729
ShMcK <shawn.j.mckay@gmail.com>
2016-05-28 11:54:36 -0700

    init

--- src/highlight.ts
+++ src/highlight.ts
@@ -1,9 +1,10 @@
 import * as Highlights from 'highlights';
 
 let grammars = {};
-if (atom && atom.grammars) {
+if (global.atom && atom.grammars) {
   grammars.registry = atom.grammars;
 }
+
 const highlighter = new Highlights(grammars);
 
 export default function highlight(code: string, lang: string) {
